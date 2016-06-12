4ce51bba5a95eb0d7683a1321d33cb735556fe85
linxi <linxi@fulongtech.cn>
2016-06-12 10:53:50 +0800

    实现网络检测

--- PerfRecoderLib/ProcessResourceUsage.cpp
+++ PerfRecoderLib/ProcessResourceUsage.cpp
@@ -1,11 +1,15 @@
 #include "stdafx.h"
 #include "ProcessResourceUsage.hpp"
-#include <memory>
+#include <numeric>
+#include <boost/algorithm/string.hpp>
 #include "CpuUsage.hpp"
+#include "PcapSource.h"
+#include "PortCache.h"
 
 ResourceUsage::ResourceUsage()
 	: CpuKernelDelta(new PH_UINT64_DELTA)
 	, CpuUserDelta(new PH_UINT64_DELTA)
+	, prevTime(std::chrono::high_resolution_clock::now())
 {
 
 }
@@ -25,11 +29,13 @@ ResourceUsage::~ResourceUsage()
 
 ProcessResourceUsage::ProcessResourceUsage()
 {
-	PhpInit();
+	init();
 }
 
 ProcessResourceUsage::~ProcessResourceUsage()
 {
+	m_networkThreadStop = true;
+	m_networkThread.join();
 }
 
 ProcessResourceUsage & ProcessResourceUsage::getInstance()
@@ -43,18 +49,57 @@ ProcessResourceUsage & ProcessResourceUsage::getInstance()
 void ProcessResourceUsage::record()
 {
 	recordCpuUsage();
+	recordNetworkUsage();
 }
 
 void ProcessResourceUsage::addProcess(DWORD processId)
 {
 	std::shared_ptr<ResourceUsage> usage(new ResourceUsage);
 	usage->processId = processId;
+	std::lock_guard<std::mutex> lock(m_resourceUsagesMutex);
 	m_resourceUsages.insert(std::make_pair(processId, usage));
+}
+
+const std::vector<std::wstring> & ProcessResourceUsage::getNetworkInterfaceNames() const
+{
+	return m_networkInterfaceNames;
+}
+
+void ProcessResourceUsage::selectNetworkInterface(int index)
+{
+	if (index < 0)
+		return;
+	m_networkInterfaceIndex = index;
+	m_reconnect = true;
+}
+
+void ProcessResourceUsage::init()
+{
+	PhpInit();
+
+	m_pcap.reset(new PcapSource);
+	m_portCache.reset(new PortCache);
+
+	m_pcap->Initialize();
+	auto count = m_pcap->EnumDevices();
+	bool isSelected = false;
+	for (decltype(count) i = 0; i < count; ++i) {
+		m_networkInterfaceNames.push_back(m_pcap->GetDeviceName(i));
+		if (!isSelected && !boost::contains(m_networkInterfaceNames.back(), L"Loop")) {
+			m_pcap->SelectDevice(i);
+			m_networkInterfaceIndex = i;
+			isSelected = true;
+		}
+	}
+	m_reconnect = false;
+
+	m_networkThreadStop = false;
+	m_networkThread = std::thread(&ProcessResourceUsage::networkThreadRun, this);
 }
 
 void ProcessResourceUsage::recordCpuUsage()
 {
-	PhpUpdateCpuInformation(TRUE, &sysTotalTime);
+	PhpUpdateCpuInformation(TRUE, &m_sysTotalTime);
 
 	PVOID processes;
 	PSYSTEM_PROCESS_INFORMATION process;
@@ -64,20 +109,23 @@ void ProcessResourceUsage::recordCpuUsage()
 	process = PH_FIRST_PROCESS(processes);
 	do {
 		DWORD processId = (DWORD)process->UniqueProcessId;
+
+		std::unique_lock<std::mutex> lock(m_resourceUsagesMutex);
 		auto iter = m_resourceUsages.find(processId);
 		if (iter != m_resourceUsages.end()) {
-			auto &usage = *iter->second;
+			auto usage = iter->second;
+			lock.unlock();
 
-			PhUpdateDelta(usage.CpuKernelDelta, process->KernelTime.QuadPart);
-			PhUpdateDelta(usage.CpuUserDelta, process->UserTime.QuadPart);
+			PhUpdateDelta(usage->CpuKernelDelta, process->KernelTime.QuadPart);
+			PhUpdateDelta(usage->CpuUserDelta, process->UserTime.QuadPart);
 
 			FLOAT newCpuUsage;
 			FLOAT kernelCpuUsage;
 			FLOAT userCpuUsage;
-			kernelCpuUsage = (FLOAT)usage.CpuKernelDelta->Delta / sysTotalTime;
-			userCpuUsage = (FLOAT)usage.CpuUserDelta->Delta / sysTotalTime;
+			kernelCpuUsage = (FLOAT)usage->CpuKernelDelta->Delta / m_sysTotalTime;
+			userCpuUsage = (FLOAT)usage->CpuUserDelta->Delta / m_sysTotalTime;
 			newCpuUsage = kernelCpuUsage + userCpuUsage;
-			usage.cpuUsage = newCpuUsage * 100.0f;
+			usage->cpuUsage = newCpuUsage * 100.0f;
 		}
 	} while (process = PH_NEXT_PROCESS(process));
 
@@ -92,5 +140,81 @@ void ProcessResourceUsage::recordCpuUsage()
 
 void ProcessResourceUsage::recordNetworkUsage()
 {
+	std::vector<decltype(m_resourceUsages)::key_type> pids;
+	{
+		std::lock_guard<std::mutex> lock(m_resourceUsagesMutex);
+		for (auto &item : m_resourceUsages)
+			pids.push_back(item.first);
+	}
 
+	for (auto pid : pids) {
+		std::unique_lock<std::mutex> lock(m_resourceUsagesMutex);
+		auto iter = m_resourceUsages.find(pid);
+		if (iter == m_resourceUsages.end())
+			continue;
+
+		auto resource = iter->second;
+		lock.unlock();
+
+		auto nowTime = std::chrono::high_resolution_clock::now();
+		decltype(nowTime) prevTime;
+
+		std::vector<NetworkTransmittedSize> sizes;
+		{
+			std::lock_guard<std::mutex> sizesLock(resource->networkMutex);
+			sizes.swap(resource->networkTransmittedSize);
+			prevTime = resource->prevTime;
+			resource->prevTime = nowTime;
+		}
+
+		std::chrono::duration<double> duration = std::chrono::duration_cast<decltype(duration)>(nowTime - prevTime);
+
+		NetworkTransmittedSize total;
+		total = std::accumulate(sizes.begin(), sizes.end(), total);
+		InterlockedExchange(&resource->m_recvSpeed, total.recv / duration.count());
+		InterlockedExchange(&resource->m_sendSpeed, total.send / duration.count());
+	}
+}
+
+void ProcessResourceUsage::networkThreadRun()
+{
+	while (!m_networkThreadStop)
+	{
+		if (m_reconnect) {
+			m_reconnect = false;
+			long index = 0;
+			InterlockedExchange(&index, m_networkInterfaceIndex);
+			if (!m_pcap->SelectDevice(index))
+				continue;
+		}
+	
+		bool timeout = false;
+		PacketInfo packetInfo;
+		if (!m_pcap->Capture(&packetInfo, &timeout)) {
+			if (!timeout)
+				m_pcap->Reconnect(m_networkInterfaceIndex);
+			continue;
+		}
+	
+		int pid = 0;
+		if (packetInfo.trasportProtocol == TRA_TCP)
+			pid = m_portCache->GetTcpPortPid(packetInfo.local_port);
+		else if (packetInfo.trasportProtocol == TRA_UDP)
+			pid = m_portCache->GetUdpPortPid(packetInfo.local_port);
+
+		std::unique_lock<std::mutex> resourceLock(m_resourceUsagesMutex);
+		auto iter = m_resourceUsages.find(pid);
+		if (iter == m_resourceUsages.end())
+			continue;
+		auto resource = iter->second;
+		resourceLock.unlock();
+
+		NetworkTransmittedSize size;
+		if (packetInfo.dir = DIR_UP)
+			size.send = packetInfo.size;
+		else if (packetInfo.dir = DIR_DOWN)
+			size.recv = packetInfo.size;
+		std::lock_guard<std::mutex> sizeLock(resource->networkMutex);
+		resource->networkTransmittedSize.push_back(size);
+	}
 }
--- PerfRecoderLib/inc/ProcessResourceUsage.hpp
+++ PerfRecoderLib/inc/ProcessResourceUsage.hpp
@@ -1,16 +1,50 @@
 #pragma once
 #include <map>
 #include <memory>
+#include <vector>
+#include <string>
+#include <mutex>
+#include <thread>
+#include <chrono>
+#include <ratio>
 
 struct PH_UINT64_DELTA;
+class PcapSource;
+class PortCache;
+
+struct NetworkTransmittedSize
+{
+	uint64_t recv;
+	uint64_t send;
+	NetworkTransmittedSize() : recv(0), send(0) {}
+	NetworkTransmittedSize &operator+=(const NetworkTransmittedSize &oth)
+	{
+		recv += oth.recv;
+		send += oth.send;
+		return *this;
+	}
+	NetworkTransmittedSize operator+(const NetworkTransmittedSize &oth)
+	{
+		NetworkTransmittedSize nt;
+		nt.recv = recv + oth.recv;
+		nt.send = send + oth.send;
+		return nt;
+	}
+};
 
 struct ResourceUsage
 {
 	DWORD processId;
 	float cpuUsage;
+	uint64_t m_recvSpeed;
+	uint64_t m_sendSpeed;
 
 	PH_UINT64_DELTA *CpuKernelDelta;
 	PH_UINT64_DELTA *CpuUserDelta;
+
+	std::vector<NetworkTransmittedSize> networkTransmittedSize;
+	std::mutex networkMutex;
+	std::chrono::high_resolution_clock::time_point prevTime;
 
 	ResourceUsage();
 	~ResourceUsage();
@@ -24,13 +58,26 @@ public:
 	static ProcessResourceUsage &getInstance();
 	void record();
 	void addProcess(DWORD processId);
+	const std::vector<std::wstring> &getNetworkInterfaceNames() const;
+	void selectNetworkInterface(int index);
 
 private:
+	void init();
 	void recordCpuUsage();
 	void recordNetworkUsage();
+	void networkThreadRun();
 
 private:
-	ULONG64 sysTotalTime;
+	ULONG64 m_sysTotalTime;
+
+	bool m_networkThreadStop;
+	std::thread m_networkThread;
+	std::shared_ptr<PcapSource> m_pcap;
+	std::shared_ptr<PortCache> m_portCache;
+	std::vector<std::wstring> m_networkInterfaceNames;
+	long m_networkInterfaceIndex;
+	bool m_reconnect;
 
 	std::map<DWORD, std::shared_ptr<ResourceUsage>> m_resourceUsages;
+	std::mutex m_resourceUsagesMutex;
 };
